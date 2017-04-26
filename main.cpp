8d3c8ec21c9e3e37379820fba384382c93ebb809
Daniel Feist <daniel.feist@mail.de>
2017-04-26 18:03:42 +0200

    Added main with usage example

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <random>
+#include <iostream>
+#include <algorithm>
+
+#include "median_of_medians.h"
+
+namespace sorter
+{ 
+
+template<class It, class Cmp>
+void insertion_sort(It first, It last, Cmp cmp)
+{
+    for (auto begin = first; begin != last; ++begin) 
+    {
+        const auto insertion = std::upper_bound(first, begin, *begin, cmp);
+        std::rotate(insertion, begin, std::next(begin));
+    }
+}
+
+template<class It>
+void insertion_sort(It first, It last)
+{
+	insertion_sort(first, last, std::less<>());
+}
+
+} // namespace helper
+
+int main()
+{
+	std::vector<int> input {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int groupsize[] = {3,5,7,11};
+    
+    for (int i : groupsize)
+    {
+        auto result = detail::median_of_medians(input.begin(), input.end(), i, sorter::insertion_sort);
+        std::cout << "groupsize=" << i << " ,median of medians=" << *result << "\n";
+    }
+
+    std::cout << '\n';
+
+    std::vector<int> input2(20);
+    std::iota(input2.begin(), input2.end(), 0);
+    
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(input2.begin(), input2.end(), g);
+
+    for (int i : groupsize)
+    {
+        auto result = detail::median_of_medians(input.begin(), input.end(), i);
+        std::cout << "groupsize=" << i << " ,median of medians=" << *result << "\n";
+    }
+}
